d4f978b9009cf9a0c92ef4438c3aa4a8584c76e6
Ingrid <ingridhong@gmail.com>
2014-10-05 23:58:39 -0700

    fixed migration problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005214116) do
+ActiveRecord::Schema.define(version: 20141006062342) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.integer  "age"
-    t.string    "major"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "age"
+    t.string   "major"
   end
 
 end
